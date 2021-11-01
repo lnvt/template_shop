6f27abd73bc5a31ce09af1e19bf13623a4959320
ThangLe <ThangLe@DESKTOP-TA83UJ7>
2021-11-01 22:39:53 +0700

    First commit

---
+++ main/js/script.js
@@ -0,0 +1,6 @@
+var username = document.getElementById('inputEmail').value
+document.getElementById('inputPassword').value;
+// document.getElementsByClassName('')
+// document.getElementsByTagName
+
+alert(username)
